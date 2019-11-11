9c3f843a7544b8d6f2ec7496cf7c7a49aec927f8
unknown <junicodefire@gmail.com>
2019-11-11 19:46:12 +0100

    for hosting

--- app/Http/Controllers/Auth/SocialAuthGoogleController.php
+++ app/Http/Controllers/Auth/SocialAuthGoogleController.php
@@ -18,29 +18,34 @@ class SocialAuthGoogleController extends Controller
 
     public function callback()
     {
-        try {
-            
+        $state = $request->query('t');
+        if($state == 'state') {
+            try {
         
-            $googleUser = Socialite::driver('google')->user();
-            $existUser = User::where('email',$googleUser->email)->first();
-            
-
-            if($existUser) {
-                Auth::loginUsingId($existUser->id);
+                $googleUser = Socialite::driver('google')->user();
+                dd($googleUser);
+                $existUser = User::where('email',$googleUser->email)->first();
+                
+    
+                if($existUser) {
+                    Auth::loginUsingId($existUser->id);
+                }
+                else {
+                    $user = new User;
+                    $user->name = $googleUser->name;
+                    $user->email = $googleUser->email;
+                    $user->google_id = $googleUser->id;
+                    $user->password = md5(rand(1,10000));
+                    $user->save();
+                    Auth::loginUsingId($user->id);
+                }
+                return redirect()->to('/home');
+            } 
+            catch (Exception $e) {
+                return 'error';
             }
-            else {
-                $user = new User;
-                $user->name = $googleUser->name;
-                $user->email = $googleUser->email;
-                $user->google_id = $googleUser->id;
-                $user->password = md5(rand(1,10000));
-                $user->save();
-                Auth::loginUsingId($user->id);
-            }
-            return redirect()->to('/home');
-        } 
-        catch (Exception $e) {
-            return 'error';
+        }else {
+            return view('welcome');
         }
     }
 }
